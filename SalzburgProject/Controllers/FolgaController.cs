85cd7e0b27557dd7e176965c380f951e3118a9f7
Hugo Machado <hugopimentamarinho@gmail.com>
2023-09-08 17:37:59 -0300

    View Create Folga

--- SalzburgProject/Controllers/FolgaController.cs
+++ SalzburgProject/Controllers/FolgaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using SalzburgProject.Data;
 using SalzburgProject.Interface;
 using SalzburgProject.Models;
@@ -9,15 +10,39 @@ namespace SalzburgProject.Controllers
     public class FolgaController : Controller
     {
         private readonly IFolgaRepository _folgaRepository;
+        private readonly IColaboradorRepository _colaboradorRepository;
 
-        public FolgaController(IFolgaRepository folgaRepository) 
+        public FolgaController(IFolgaRepository folgaRepository, IColaboradorRepository colaboradorRepository) 
         { 
             _folgaRepository = folgaRepository;
+            _colaboradorRepository = colaboradorRepository;
         }
         public async Task<IActionResult> Index()
         {
             IEnumerable<Folga> folgas = await _folgaRepository.GetAll();
             return View(folgas);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            IEnumerable<Colaborador> colaboradores = await _colaboradorRepository.GetAll();
+
+            // Cria a SelectList para a ViewBag, usando a lista de colaboradores
+            ViewBag.ColaboradorList = new SelectList(colaboradores, "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Folga folga)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(folga);
+            }
+            _folgaRepository.Add(folga);
+            return RedirectToAction("Index");
         }
     }
 }
