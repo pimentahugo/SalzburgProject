29975d13bad2d41e40075317c0f3830e597aef92
Hugo Machado <hugopimentamarinho@gmail.com>
2023-09-09 18:31:52 -0300

    Campo CPF

--- SalzburgProject/Migrations/20230909213006_CampoCPF.Designer.cs
+++ SalzburgProject/Migrations/20230909213006_CampoCPF.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalzburgProject.Data;
 
@@ -11,9 +12,10 @@ using SalzburgProject.Data;
 namespace SalzburgProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230909213006_CampoCPF")]
+    partial class CampoCPF
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SalzburgProject/Migrations/20230909213006_CampoCPF.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalzburgProject.Migrations
+{
+    public partial class CampoCPF : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChavesPix_Colaboradores_ColaboradorId",
+                table: "ChavesPix");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CPF",
+                table: "Colaboradores",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "ChavesPix",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KeyPix",
+                table: "ChavesPix",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ColaboradorId",
+                table: "ChavesPix",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChavesPix_Colaboradores_ColaboradorId",
+                table: "ChavesPix",
+                column: "ColaboradorId",
+                principalTable: "Colaboradores",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChavesPix_Colaboradores_ColaboradorId",
+                table: "ChavesPix");
+
+            migrationBuilder.DropColumn(
+                name: "CPF",
+                table: "Colaboradores");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "ChavesPix",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KeyPix",
+                table: "ChavesPix",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ColaboradorId",
+                table: "ChavesPix",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChavesPix_Colaboradores_ColaboradorId",
+                table: "ChavesPix",
+                column: "ColaboradorId",
+                principalTable: "Colaboradores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
