d2445f4ce15e9ea6349983ffc229c5f055b16120
Hugo Machado <hugopimentamarinho@gmail.com>
2023-09-13 00:28:09 -0300

    Inclusao de modelagem para tela de custo

---
+++ SalzburgProject/Models/Custo.cs
@@ -0,0 +1,20 @@
+﻿using SalzburgProject.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalzburgProject.Models
+{
+    public class Custo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public TipoCusto TipoCusto { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+    }
+}
---
+++ SalzburgProject/Models/Enum/TipoCusto.cs
@@ -0,0 +1,11 @@
+﻿namespace SalzburgProject.Models.Enum
+{
+    public enum TipoCusto
+    {
+        CustoOperacional = 1,
+        Investimento = 2,
+        Logistica = 3,
+        Funcionario = 4,
+        Imposto = 5
+    }
+}
